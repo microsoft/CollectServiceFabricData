e7dcaa557ad4af3ea21e3cacb84e9a8a1a3b1d46
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-09-22 08:20:24 -0400

    nuget updates

--- scripts/nuget-gpr-test.ps1
+++ scripts/nuget-gpr-test.ps1
@@ -1,10 +1,13 @@
-# script to test nuget package from nuget.org
+# script to test nuget package from github package registry gpr
 
 param(
     $projectName = "Microsoft.ServiceFabric.Support.CollectSFData",
-    $nugetSource = "https://api.nuget.org/v3/index.json",
     $outputDirectory = "$($env:USERPROFILE)\downloads\$($ProjectName)",
-    $apiKey
+    $user,
+    $owner = $user,
+    $nugetSource = "https://nuget.pkg.github.com/$owner/index.json",
+    $pat,
+    $apiKey = "gprkey"
 )
 
 $currentLocation = get-location
@@ -15,7 +18,7 @@ if(![io.file]::exists("$pwd\nuget.exe"))
     (new-object net.webclient).downloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", "$PSScriptRoot\nuget.exe")
 }
 
-# .\nuget.exe sources Add -Name $projectName -Source $nugetSource
+.\nuget.exe sources Add -Name "GPR" -Source $nugetSource -UserName $user -Password $pat -Verbosity detailed
 # .\nuget.exe setapikey $apiKey -Source "GPR" -Verbosity detailed
 # .\nuget.exe pack "..\$($projectName)\$($projectName).nuspec" -OutputDirectory $outputDirectory -apikey $apiKey
 
@@ -23,7 +26,7 @@ if(![io.file]::exists("$pwd\nuget.exe"))
 write-host ".\nuget install $projectName -Source $nugetSource -outputdirectory $outputDirectory -verbosity detailed -prerelease"
 .\nuget.exe install $projectName -Source $nugetSource -outputdirectory $outputDirectory -verbosity detailed -prerelease
 
-# .\nuget.exe sources Remove -Name $projectName -Source $nugetSource
+.\nuget.exe sources Remove -Name "GPR" -Source $nugetSource
 tree /a /f $outputDirectory
 $exePath = @((get-childitem -Path $outputDirectory -Filter "collectsfdata.exe" -Recurse | ? FullName -match "bin.+release").FullName)[-1]
 . $exePath
