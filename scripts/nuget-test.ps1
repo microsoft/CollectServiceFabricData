1f0fd988762db76cf141eb50abd7c75654787b40
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-09-23 09:20:19 -0400

    kusto rest testing

--- scripts/nuget-test.ps1
+++ scripts/nuget-test.ps1
@@ -3,31 +3,30 @@
 param(
     $projectName = "Microsoft.ServiceFabric.Support.CollectSFData",
     $nugetSource = "https://api.nuget.org/v3/index.json",
+    $nugetDownloadUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe",
     $outputDirectory = "$($env:USERPROFILE)\downloads\$($ProjectName)",
     $apiKey
 )
 
-$currentLocation = get-location
-set-location $PSScriptRoot
+$env:path += ";$pwd;$psscriptroot"
 
-if(![io.file]::exists("$pwd\nuget.exe"))
+if(!(test-path nuget))
 {
-    (new-object net.webclient).downloadFile("https://dist.nuget.org/win-x86-commandline/latest/nuget.exe", "$PSScriptRoot\nuget.exe")
+    (new-object net.webclient).downloadFile($nugetDownloadUrl, "$pwd\nuget.exe")
 }
 
-# .\nuget.exe sources Add -Name $projectName -Source $nugetSource
-# .\nuget.exe setapikey $apiKey -Source "GPR" -Verbosity detailed
-# .\nuget.exe pack "..\$($projectName)\$($projectName).nuspec" -OutputDirectory $outputDirectory -apikey $apiKey
+# nuget sources Add -Name $projectName -Source $nugetSource
+# nuget setapikey $apiKey -Source "GPR" -Verbosity detailed
+# nuget pack "..\$($projectName)\$($projectName).nuspec" -OutputDirectory $outputDirectory -apikey $apiKey
 
 [io.directory]::createDirectory($outputDirectory)
 write-host ".\nuget install $projectName -Source $nugetSource -outputdirectory $outputDirectory -verbosity detailed -prerelease"
-.\nuget.exe install $projectName -Source $nugetSource -outputdirectory $outputDirectory -verbosity detailed -prerelease
+nuget install $projectName -Source $nugetSource -outputdirectory $outputDirectory -verbosity detailed -prerelease
 
-# .\nuget.exe sources Remove -Name $projectName -Source $nugetSource
+# nuget sources Remove -Name $projectName -Source $nugetSource
 tree /a /f $outputDirectory
 $exePath = @((get-childitem -Path $outputDirectory -Filter "collectsfdata.exe" -Recurse | ? FullName -match "bin.+release").FullName)[-1]
 . $exePath
-set-location $currentLocation
 write-host "install / output dir: $($outputDirectory)" -ForegroundColor Green
 
 
