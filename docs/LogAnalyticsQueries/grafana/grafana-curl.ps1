4098e304c938ac6ad43db04840850c964466b83d
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-10-06 15:39:04 -0400

    cleanup documentation.

--- docs/LogAnalyticsQueries/grafana/grafana-curl.ps1
+++
@@ -1,6 +0,0 @@
-$apiToken = ""
-$dashboard = "https://jagilber.grafana.net/api/dashboards/uid/xxxxxxxxx"
-$result = curl.exe -H "Authorization: Bearer $apiToken" $dashboard
-$global:grafanaSchema = $result | convertfrom-json | convertto-json -depth 99
-$global:grafanaSchema
-write-host "json saved in `$global:grafanaSchema"
