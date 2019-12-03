963efd759f2e38c4728bd2f81f6134f2694443e1
Jason Gilbertson <jagilber@users.noreply.github.com>
2019-12-02 20:46:59 -0500

    fix for table Timestamp being local and not utc

--- CollectSFData/Azure/TableManager.cs
+++ CollectSFData/Azure/TableManager.cs
@@ -238,7 +238,7 @@ namespace CollectSFData
                     TotalRecords++;
                     results.Add(new CsvTableRecord()
                     {
-                        Timestamp = result.Timestamp.DateTime,
+                        Timestamp = result.Timestamp.DateTime.ToUniversalTime(),
                         EventTimeStamp = actualTimeStamp,
                         ETag = result.ETag,
                         PartitionKey = $"\"{result.PartitionKey}\"",
