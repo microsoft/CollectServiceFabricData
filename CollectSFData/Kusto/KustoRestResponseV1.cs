a99e50d605927e38204cfd3b31754a2008c32033
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-04-12 19:11:54 -0400

    add license header

--- CollectSFData/Kusto/KustoRestResponseV1.cs
+++ CollectSFData/Kusto/KustoRestResponseV1.cs
@@ -1,5 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
 
 namespace CollectSFData
 {
