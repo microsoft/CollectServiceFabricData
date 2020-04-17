927364a8a3ab186533c15b077599c37ab2b21369
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-04-17 09:23:08 -0400

    adding tests and configurations

--- CollectSFDataTests/Common/HttpTests.cs
+++ CollectSFDataTests/Common/HttpTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CollectSFData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CollectSFDataTests.Common
+namespace CollectSFData.Tests
 {
-    class HttpTests
+    [TestClass()]
+    public class HttpTests
     {
+        [TestMethod()]
+        public void ClientFactoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SendRequestTest()
+        {
+            Assert.Fail();
+        }
     }
 }
