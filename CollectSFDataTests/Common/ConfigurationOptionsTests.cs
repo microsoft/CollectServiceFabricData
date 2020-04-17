3f400750a77b06ed8a2c5b4aafd129ba67ba058f
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-04-17 09:23:08 -0400

    adding tests and configurations

--- CollectSFDataTests/Common/ConfigurationOptionsTests.cs
+++ CollectSFDataTests/Common/ConfigurationOptionsTests.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CollectSFData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CollectSFDataTests.Common
+namespace CollectSFData.Tests
 {
-    class ConfigurationOptionsTests
+    [TestClass()]
+    public class ConfigurationOptionsTests
     {
+        [TestMethod()]
+        public void CheckReleaseVersionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConfigurationOptionsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ConvertFileTypeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisplayStatusTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsCacheLocationPreConfiguredTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsClientIdConfiguredTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsKustoConfiguredTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsKustoPurgeRequestedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsLogAnalyticsConfiguredTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsLogAnalyticsPurgeRequestedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PopulateConfigTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveConfigFileTest()
+        {
+            Assert.Fail();
+        }
     }
 }
--- CollectSFDataTests/Common/ProgramTests.cs
+++ CollectSFDataTests/Common/ProgramTests.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CollectSFData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CollectSFDataTests;
 
-namespace CollectSFDataTests.Common
+namespace CollectSFData.Tests
 {
-    class ProgramTests
+    [TestClass()]
+    public class ProgramTests
     {
+        static ProgramTests()
+        {
+            // todo
+            //TestUtilities.BuildWindowsCluster();
+        }
+
+        [TestMethod()]
+        public void DetermineClusterIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DownloadAzureDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteTest()
+        {
+            string[] args = new string[1] { $"{TestUtilities.TestConfigurationsDir}\\collectsfdata.options.json" };
+            Program program = new Program();
+            int result = program.Execute(args);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod()]
+        public void FinalizeKustoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InitializeKustoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void InitializeLogAnalyticsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MainTest()
+        {
+            Program program = new Program();
+            Assert.IsNotNull(program);
+        }
+
+        [TestMethod()]
+        public void QueueForIngestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UploadCacheDataTest()
+        {
+            Assert.Fail();
+        }
     }
 }
