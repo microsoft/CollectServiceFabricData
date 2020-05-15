79ef7d3780f29c039f00742839f1633d1757d137
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-05-15 08:50:42 -0400

    test blob creation confirmed

--- CollectSFDataTests/TestUtilities/setup-test-env.ps1
+++ CollectSFDataTests/TestUtilities/setup-test-env.ps1
@@ -6,7 +6,7 @@ setup variables for running tests
 param(
     [switch]$clean,
     [switch]$reset,
-    [string]$configurationFile = "$psscriptroot\..\bin\temp\collectSfDataTestProperties.json",
+    [string]$configurationFile = "$env:LOCALAPPDATA\collectsfdata\collectSfDataTestProperties.json", # "$psscriptroot\..\bin\temp\collectSfDataTestProperties.json",
     [string]$tempDir = "$psscriptroot\..\bin\temp"
 )
 
@@ -16,6 +16,7 @@ $error.clear()
 
 class TestSettings {
     TestSettings() {}
+        # needed ? not currently used
         $testAadUser = $null
         $testAadPassword = $null
         $testAadKeyVault = $null
@@ -23,6 +24,9 @@ class TestSettings {
         $testAadSourceVaultValue = $null
         $aadCertificateUrlValue = $null
       
+        # for file download and gather tests
+        $testAzStorageAccount = "collectsfdatatests"
+
         # for azure cluster deployments
         $adminUserName = $null
         $adminPassword = $null
@@ -49,6 +53,62 @@ class TestEnv {
         $this.CheckTempDir()
         $this.CheckTemplate()
         $this.ReadConfig($this.configurationFile)
+        $this.CheckAzureConfig()
+    }
+
+    [void] CheckAzureConfig() {
+        $settings = $this.testSettings
+        if(!$settings.AzureClientId -or !$settings.AzureClientSecret -or !$settings.AzureResourceGroup -or !$settings.AzureResourceGroupLocation){
+            Write-Warning "azure settings not configured. storage tests may fail"
+            return
+        }
+
+        write-host "checking azure config" -ForegroundColor Cyan
+        $credential = new-object -typename System.Management.Automation.PSCredential `
+            -argumentlist @(
+                $settings.AzureClientId, 
+                ($settings.AzureClientSecret | convertto-securestring -Force -AsPlainText)
+            )
+
+        Login-AzAccount -TenantId $settings.AzureTenantId -Credential $credential -ServicePrincipal
+        get-azcontext | fl *
+
+        write-host "checking resource group $($settings.AzureResourceGroup)"
+        if(!(get-azresourcegroup $settings.AzureResourceGroup -Location $settings.AzureResourceGroupLocation)){
+            if(!(new-azresourcegroup $settings.AzureResourceGroup -Location $settings.AzureResourceGroupLocation)){
+                throw new-object System.UnauthorizedAccessException($error | out-string)
+            }
+        }
+
+        $settings.testAzStorageAccount = "$($settings.testAzStorageAccount)$([math]::Abs($settings.AzureClientId.GetHashCode()))".Substring(0, 24)
+        write-host "setting unique storage account $($settings.testAzStorageAccount)"
+        $this.SaveConfig()
+
+        write-host "checking storage account $($settings.testAzStorageAccount)"
+        $sa = get-azstorageaccount $settings.testAzStorageAccount -resourcegroupname $settings.AzureResourceGroup
+        if(!($sa)){
+            if(!(new-azstorageaccount $settings.testAzStorageAccount -skuname 'Standard_LRS' -resourcegroupname $settings.AzureResourceGroup -Location $settings.AzureResourceGroupLocation)){
+                throw new-object System.UnauthorizedAccessException($error | out-string)
+            }
+        }
+
+        write-host "getting key $($settings.testAzStorageAccount)"
+        $sk = Get-AzStorageAccountKey -ResourceGroupName $settings.AzureResourceGroup -Name $settings.testAzStorageAccount
+        $sc = new-azstoragecontext -storageaccountname $settings.testAzStorageAccount -storageaccountkey ([convert]::Tobase64String([text.encoding]::Unicode.GetBytes($sk.Value)))
+        $st = New-AzStorageAccountSASToken -ResourceType Container,Service,Object `
+            -Permission 'racwdlup' `
+            -Protocol HttpsOnly `
+            -StartTime (get-date).AddMinutes(-5) `
+            -ExpiryTime (get-date).AddHours(8) `
+            -Context $sc `
+            -Service Blob,Table,File
+
+        $global:sasuri = "$($sa.Context.BlobEndPoint)$($st)"
+        $global:storageAccount = $sa
+        write-host "setting test token $global:sasUri"
+        $settings.SasKey = $global:sasuri
+        $this.SaveConfig()
+
     }
 
     [void] CheckTempDir(){
@@ -62,7 +122,9 @@ class TestEnv {
         if (!(test-path $this.configurationFile) -or $this.reset) {
             $this.SaveConfig()
             write-host "edit file directly and save: $this.configurationFile" -foregroundcolor green
-            start . $this.configurationFile
+            write-host "create azure app id / spn for azure storage / gather tests. .\azure-az-create-aad-application-spn.ps1 can be used to create one progammatically."
+            write-host ".\azure-az-create-aad-application-spn.ps1 -aadDisplayName collectsfdata -uri http://collectsfdata"
+            . $this.configurationFile
         }
     }
 
@@ -74,7 +136,7 @@ class TestEnv {
         $this.testSettings = get-content -raw $configurationFile | convertfrom-json
         write-host "current configuration:`r`n$($global:testProperties | out-string)" -foregroundcolor green
         write-host "current configuration saved in `$global:testProperties" -foregroundcolor green
-        
+
         return $this.testSettings
     }
 
@@ -83,11 +145,17 @@ class TestEnv {
     }
 
     [bool] SaveConfig([TestSettings] $settings = $this.testSettings) {
+       
+        $configDir = [io.path]::GetDirectoryName($this.configurationFile)
+        if(!(test-path $configDir)){
+            New-Item -ItemType Directory -Path $configDir
+        }
+        
         $error.Clear()
         out-file -inputobject ($settings | convertto-json) -filepath $this.configurationFile
-        write-host "current configuration:`r`n$($settings | out-string)" -foregroundcolor green
+        write-host "current configuration:`r`n$($settings | convertto-json -depth 5)" -foregroundcolor green
         write-host "current configuration saved in $($this.configurationFile)" -foregroundcolor green
-    
+
         return !$error
     }
 }
@@ -101,5 +169,6 @@ if ($error) {
 }
 else {
     write-host ($testEnv | Get-Member | out-string)
+    write-host "current configuration:`r`n$($testenv | convertto-json -depth 5)" -foregroundcolor green
     write-host "use `$global:testEnv object" -ForegroundColor Green
 }
