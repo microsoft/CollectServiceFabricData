8f6655d6f9f2a04e780d5b109a1b7dfb6cc644bb
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-05-15 08:50:42 -0400

    test blob creation confirmed

--- CollectSFDataTests/TestUtilities/kusto-blob-test-generator.ps1
+++ CollectSFDataTests/TestUtilities/kusto-blob-test-generator.ps1
@@ -8,58 +8,53 @@
 #>
 
 param(
-    [Parameter(Mandatory = $false)]
-    $BlobUri = "",
-    $baseContainerName = "kustoblobtest",
+    [Parameter(Mandatory = $true)]
+    $BlobSasUri = "",
+    $baseContainerName = "sflogsblobtest",
     $directoryDepth = 3,
     $numberOfContainers = 6,
     $numberOfDirectories = 3,
     $numberOfBlobs = 100,
     [switch]$recreateBaseContainer
 )
-
+$PSModuleAutoLoadingPreference = 2
+$ErrorActionPreference = "continue"
 Clear-Host
 $error.Clear()
-$ErrorActionPreference = "silentlycontinue"
 $totalBlobs = 0
 $startTime = get-date
 import-module azure.storage
+$ErrorActionPreference = "stop"
 
-function main()
-{
+function main() {
     write-host "creating $($numberOfBlobs * $numberOfDirectories * ($directoryDepth + 1)) blobs"
     #[microsoft.windowsazure.storage.blob.cloudBlobContainer]
-    $rootContainer = new-object microsoft.windowsazure.storage.blob.CloudBlobContainer(new-object Uri($BlobUri));
+    $rootContainer = new-object microsoft.windowsazure.storage.blob.CloudBlobContainer(new-object Uri($BlobSasUri));
     # Get the ServiceClient from the RootContainer ref
     $baseContainer = $rootContainer.ServiceClient.GetContainerReference($baseContainerName)
 
-    if ($recreateBaseContainer -and $baseContainer.DeleteIfExists())
-    {
+    if ($recreateBaseContainer -and $baseContainer.DeleteIfExists()) {
         write-host "deleting existing container $($baseContainerName)"
     }
     
     $error.Clear()
 
-    while ($true)
-    {
-        [void]$baseContainer.CreateIfNotExists() 
+    while ($true) {
+        $result = $baseContainer.CreateIfNotExistsAsync().Result
+        write-host "create container result $($result | convertto-json -depth 5)"
 
-        if ($error)
-        {
-            if ($error -imatch "409")
-            {
+        if ($error) {
+            if ($error -imatch "409") {
                 write-host "." -NoNewline
             }
-            else
-            {
+            else {
                 write-host $error
             }
 
             $error.Clear()
             Start-Sleep -Seconds 1
         }
-        else
-        {
+        else {
             write-host
             write-host "recreated container $($baseContainerName)"
             break
@@ -67,44 +62,37 @@ function main()
     }
 
     # root
-    for ($containerCount = 0; $containerCount -le $numberOfContainers; $containerCount++)
-    {
+    for ($containerCount = 0; $containerCount -le $numberOfContainers; $containerCount++) {
         $containerName = "testcontainer$($containerCount)"
         $container = $baseContainer.ServiceClient.GetContainerReference($containerName)
-        [void]$container.CreateIfNotExists()
-        write-host "creating container: $($containerName)"
+        $result = $container.CreateIfNotExistsAsync().Result
+        write-host "creating container: $($containerName) result: $($result | convertto-json -depth 5)"
     }
-       
-    for ($directoryDepthCount = 1; $directoryDepthCount -le $directoryDepth; $directoryDepthCount++)
-    {
-        if ($directoryDepthCount -eq 1)
-        {
+
+    for ($directoryDepthCount = 1; $directoryDepthCount -le $directoryDepth; $directoryDepthCount++) {
+        if ($directoryDepthCount -eq 1) {
             $directoryDepthName = ""
         }
-        else
-        {
+        else {
             $directoryDepthName += "depth$($directoryDepthCount)/"
         }
 
-        for ($directoryCount = 0; $directoryCount -le $numberOfDirectories; $directoryCount++)
-        {
-            if ($directoryCount -eq 0)
-            {
+        for ($directoryCount = 0; $directoryCount -le $numberOfDirectories; $directoryCount++) {
+            if ($directoryCount -eq 0) {
                 $directoryName = ""
             }
-            else
-            {
+            else {
                 $directoryName = "$($directoryDepthName)testdirectory$($directoryCount)/"
             }
 
-            for ($blobCount = 0; $blobCount -le $numberOfBlobs; $blobCount++)
-            {
+            for ($blobCount = 0; $blobCount -le $numberOfBlobs; $blobCount++) {
                 $blobName = "$($directoryName)testblob$($totalBlobs.ToString().PadLeft(7,"0")).dtr"
                 $totalBlobs++
                 write-host "creating blob $($totalBlobs): $($blobName)"
                 $blobRef = $baseContainer.GetBlockBlobReference($blobName)
-                $blobRef.UploadText("testtest", $null, $null, $null, $null);
-            }    
+                $result = $blobRef.UploadTextAsync("testtest", $null, $null, $null, $null).Result;
+                write-host "upload file result: $($result | convertto-json -depth 5)"
+            }
         }
     }
 }
