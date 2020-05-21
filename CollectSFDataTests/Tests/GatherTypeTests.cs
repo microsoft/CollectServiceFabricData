5e3d0d8169b51d2a88316430c8f1c7dfe5f78162
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-05-21 09:21:32 -0400

    tests no longer hanging.

--- CollectSFDataTests/Tests/GatherTypeTests.cs
+++ CollectSFDataTests/Tests/GatherTypeTests.cs
@@ -6,35 +6,28 @@
 using CollectSFData.DataFile;
 using NUnit.Framework;
 using System;
+using System.Diagnostics;
 
 namespace CollectSFDataTests
 {
     [TestFixture]
-    public class GatherTypeTests
+    public class GatherTypeTests : TestUtilities
     {
         [Test]
         public void GatherTypeAnyTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.any.ToString();
-            utils.ConfigurationOptions.SasKey = TestUtilities.TestProperties.SasKey;
-            utils.ConfigurationOptions.CacheLocation = TestUtilities.TempDir;
-            utils.ConfigurationOptions.StartTimeStamp = DateTime.MinValue.ToString("o");
-            utils.ConfigurationOptions.EndTimeStamp = DateTime.Now.ToString("o");
-            //test
-            //ProcessOutput result = utils.ExecuteMoqTest();
-            //end test
-
             ProcessOutput results = utils.ExecuteTest();
+
             Assert.IsFalse(results.HasErrors(), results.ToString());
         }
 
         [Test]
         public void GatherTypeBadTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = "wtw";
-
             ProcessOutput results = utils.ExecuteTest();
 
             Assert.IsTrue(results.StandardOutput.Contains("ValidateFileType:warning: invalid -type"), results.ToString());
@@ -45,7 +38,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeCounterTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.counter.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -56,7 +49,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeExceptionTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.exception.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -66,7 +59,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeNullTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = null;
 
             ProcessOutput results = utils.ExecuteTest();
@@ -79,7 +72,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeSetupTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.setup.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -89,7 +82,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeTableTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.table.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -99,7 +92,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeTraceTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.trace.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -109,7 +102,7 @@ namespace CollectSFDataTests
         [Test]
         public void GatherTypeUnknownTest()
         {
-            TestUtilities utils = new TestUtilities();
+            TestUtilities utils = DefaultUtilities();
             utils.ConfigurationOptions.GatherType = FileTypesEnum.unknown.ToString();
 
             ProcessOutput results = utils.ExecuteTest();
@@ -117,6 +110,16 @@ namespace CollectSFDataTests
             Assert.IsTrue(results.StandardOutput.Contains("ValidateFileType:warning: invalid -type"), results.ToString());
             // should not start execution
             Assert.NotZero(results.ExitCode);
+        }
+
+        private TestUtilities DefaultUtilities()
+        {
+            TestUtilities utils = new TestUtilities();
+            utils.ConfigurationOptions.SasKey = TestUtilities.TestProperties.SasKey;
+            utils.ConfigurationOptions.CacheLocation = TestUtilities.TempDir;
+            utils.ConfigurationOptions.StartTimeStamp = DateTime.MinValue.ToString("o");
+            utils.ConfigurationOptions.EndTimeStamp = DateTime.Now.ToString("o");
+            return utils;
         }
     }
 }
