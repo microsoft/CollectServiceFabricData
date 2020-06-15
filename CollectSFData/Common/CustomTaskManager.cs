c5b95ee08884475f03ddfeaf8e46783f99fd386d
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-06-15 07:19:49 -0400

    testing max thread check

--- CollectSFData/Common/CustomTaskManager.cs
+++ CollectSFData/Common/CustomTaskManager.cs
@@ -184,6 +184,17 @@ namespace CollectSFData.Common
 
         private Task AddToQueue(TaskObject taskObject, bool taskWait = false)
         {
+            int workerThreads = 0;
+            int completionPortThreads = 0;
+            int count = 0;
+
+            do
+            {
+                Log.Debug($"waiting for available worker thread {count++}");
+                ThreadPool.GetAvailableThreads(out workerThreads, out completionPortThreads);
+                Thread.Sleep(ThreadSleepMs10);
+            } while (workerThreads < 1);
+
             QueuedTaskObjects.Add(taskObject);
             Log.Debug($"adding new taskobject to queue: {CallerName}");
             TimeSpan delay = new TimeSpan();
