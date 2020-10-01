785cb20a412e1c850d0065bd6adb7e95f82dfd4c
Jason Gilbertson <jagilber@microsoft.com>
2020-10-01 18:04:12 -0400

    switch to system.net from system.web

--- CollectSFData/Azure/SasParameters.cs
+++ CollectSFData/Azure/SasParameters.cs
@@ -4,7 +4,7 @@
 // ------------------------------------------------------------
 
 using System;
-using System.Web;
+using System.Net;
 
 namespace CollectSFData.Azure
 {
@@ -18,7 +18,7 @@ namespace CollectSFData.Azure
             {
                 string paramName = parameter.ToLower().Split('=')[0];
                 string encodedParamValue = parameter.Split('=')[1];
-                string paramValue = HttpUtility.UrlDecode(encodedParamValue);
+                string paramValue = WebUtility.UrlDecode(encodedParamValue);
 
                 if (paramName.Equals("api-version")) { ApiVersion = paramValue; }
                 if (paramName.Equals("sv")) { SignedVersion = paramValue; }
