97fa1153b75cddeaf7071adac7dad744b21db739
Jason Gilbertson <jagilber@microsoft.com>
2020-10-05 08:55:59 -0400

    add collectsfdata exe project

---
+++ CollectSFData/Program.cs
@@ -0,0 +1,17 @@
+﻿using CollectSFData.Common;
+
+
+namespace CollectSFData
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Collector collector = new Collector();
+            ConfigurationOptions sfConfig = new ConfigurationOptions();
+            
+            sfConfig.DefaultConfig();
+            return collector.Collect(args, sfConfig);
+        }
+    }
+}
