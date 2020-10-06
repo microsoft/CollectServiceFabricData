d535232d3454b3a7a1374b963fec990d7aeb5af1
Jason Gilbertson <jagilber@microsoft.com>
2020-10-06 08:24:54 -0400

    working

--- CollectSFData/Program.cs
+++ CollectSFData/Program.cs
@@ -8,9 +8,9 @@ namespace CollectSFData
         static int Main(string[] args)
         {
             Collector collector = new Collector();
-            ConfigurationOptions sfConfig = new ConfigurationOptions();
-            
+            ConfigurationOptions sfConfig = new ConfigurationOptions();           
             sfConfig.DefaultConfig();
+
             return collector.Collect(args, sfConfig);
         }
     }
