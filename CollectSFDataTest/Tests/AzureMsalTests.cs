f5c9bfbc1e53c68f71e8009a9132945f8c5f0406
Jason Gilbertson <jagilber@users.noreply.github.com>
2020-10-08 12:11:29 -0400

    adding msal logon tests

---
+++ CollectSFDataTest/Tests/AzureMsalTests.cs
@@ -0,0 +1,43 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using CollectSFData.Azure;
+using CollectSFData.DataFile;
+using NUnit.Framework;
+using System;
+
+namespace CollectSFDataTests
+{
+    [TestFixture]
+    public class AzureMsalTests : TestUtilities
+    {
+        [Test(Description = "Azure Msal Client test", TestOf = typeof(SasEndpoints))]
+        public void AzureMsalClientTest()
+        {
+            TestUtilities utils = DefaultUtilities();
+
+            ProcessOutput results = utils.ExecuteTest();
+
+            Assert.IsFalse(results.HasErrors(), results.ToString());
+        }
+
+        private TestUtilities DefaultUtilities()
+        {
+            TestUtilities utils = new TestUtilities();
+            utils.ConfigurationOptions.SasKey = TestUtilities.TestProperties.SasKey;
+            utils.ConfigurationOptions.CacheLocation = TestUtilities.TempDir;
+            utils.ConfigurationOptions.StartTimeStamp = DateTime.MinValue.ToString("o");
+            utils.ConfigurationOptions.EndTimeStamp = DateTime.Now.ToString("o");
+            utils.ConfigurationOptions.AzureClientId = TestUtilities.TestProperties.AzureClientId;
+            utils.ConfigurationOptions.AzureClientSecret = TestUtilities.TestProperties.AzureClientSecret;
+            utils.ConfigurationOptions.AzureResourceGroup = TestUtilities.TestProperties.AzureResourceGroup;
+            utils.ConfigurationOptions.AzureResourceGroupLocation = TestUtilities.TestProperties.AzureResourceGroupLocation;
+            utils.ConfigurationOptions.AzureSubscriptionId = TestUtilities.TestProperties.AzureSubscriptionId;
+            utils.ConfigurationOptions.AzureTenantId = TestUtilities.TestProperties.AzureTenantId;
+            utils.ConfigurationOptions.GatherType = FileTypesEnum.any.ToString();
+            return utils;
+        }
+    }
+}
