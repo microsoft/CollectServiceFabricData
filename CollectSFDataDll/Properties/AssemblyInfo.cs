7aa45492c6c6c718dd13b9916e0e4c92fbceaeb1
Jason Gilbertson <jagilber@microsoft.com>
2020-10-16 16:13:09 -0400

    test nuget pack

--- CollectSFDataDll/Properties/AssemblyInfo.cs
+++ CollectSFDataDll/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CollectSFData")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".net framework console utility to manage service fabric support logs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("CollectSFData")]
