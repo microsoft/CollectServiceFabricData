853072f3e784db3a179742bbacf310acc4ea5905
Jason Gilbertson <jagilber@microsoft.com>
2020-10-16 13:47:35 -0400

    add new collectsfdata exe project. rename execute() to collect()

---
+++ CollectSFData/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollectSFData;
+using CollectSFData.Common;
+
+namespace CollectSFData
+{
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            Collector collector = new Collector();
+            ConfigurationOptions options = new ConfigurationOptions();
+            //options.DefaultConfig();
+            return collector.Collect(args);
+        }
+    }
+}
