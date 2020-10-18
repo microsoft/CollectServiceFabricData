b0cbe45f652a88a13f92d15297edfa7c719ddda6
Jason Gilbertson <jagilber@microsoft.com>
2020-10-18 18:42:26 -0400

    building as framework and core using core csproj/sln

---
+++ scripts/build-clean.ps1
@@ -0,0 +1,24 @@
+$currentLocation = (get-location).Path
+
+Set-Location ..\$PSScriptRoot
+
+rd .\bin -re -fo
+rd .\obj -re -fo
+
+
+cd .\CollectSFData
+rd .\bin -re -fo
+rd .\obj -re -fo
+cd ..
+
+cd .\CollectSFDataDll
+rd .\bin -re -fo
+rd .\obj -re -fo
+cd ..
+
+cd .\CollectSFDataTest
+rd .\bin -re -fo
+rd .\obj -re -fo
+cd ..
+
+set-location $currentLocation
