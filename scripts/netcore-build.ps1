6ed3e95ebdc7d211bcc6c62618d6c2299b9bce9e
Jason Gilbertson <jagilber@microsoft.com>
2020-10-18 13:31:59 -0400

    add netcore-build.ps1

---
+++ scripts/netcore-build.ps1
@@ -0,0 +1,12 @@
+	
+dotnet publish -r win-x64 -c Release -p:PublishSingleFile=true -p:PublishedTrimmed=true
+
+dotnet publish --self-contained
+
+dotnet publish -r win-x64 -c Release --self-contained $true --no-dependencies -p:PublishSingleFile=true -p:PublishedTrimmed=true
+
+dotnet publish -r win-x64 -c Debug --self-contained $true --no-dependencies -p:PublishSingleFile=true -p:PublishedTrimmed=true
+
+# to pass args add ' -- ' to command first
+# not working in git codespaces
+dotnet run -- -h
