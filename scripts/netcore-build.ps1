280848162dff263d01cd55f0f871cf68bf19bde5
Jason Gilbertson <jagilber@microsoft.com>
2020-10-20 09:22:53 -0400

    testing multitarget build

--- scripts/netcore-build.ps1
+++ scripts/netcore-build.ps1
@@ -10,3 +10,5 @@ dotnet publish -r win-x64 -c Debug --self-contained $true --no-dependencies -p:P
 # to pass args add ' -- ' to command first
 # not working in git codespaces
 dotnet run -- -h
+
+dotnet publish .\CollectSFData.csproj -r win-x64 -c Release --no-dependencies -p:PublishedTrimmed=true
