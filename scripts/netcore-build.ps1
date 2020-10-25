e7082c5b6d6c5c185902b68b8ac6262c95dbcf0e
Jason Gilbertson <jagilber@microsoft.com>
2020-10-25 19:22:59 -0400

    testing .netcore

--- scripts/netcore-build.ps1
+++ scripts/netcore-build.ps1
@@ -11,4 +11,7 @@ dotnet publish -r win-x64 -c Debug --self-contained $true --no-dependencies -p:P
 # not working in git codespaces
 dotnet run -- -h
 
+# 20-10-25 19:19:22 not working
 dotnet publish .\CollectSFData.csproj -r win-x64 -c Release --no-dependencies -p:PublishedTrimmed=true
+
+dotnet publish .\CollectSFData\CollectSFData.csproj -f netcoreapp3.1 -r win-x64 -c Release --self-contained $true -p:PublishSingleFile=true -p:PublishedTrimmed=false
