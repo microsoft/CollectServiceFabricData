094ad12877ba9c660a1a5de52f57700cacb3fc87
Jason Gilbertson <jagilber@microsoft.com>
2020-11-04 09:36:45 -0500

    add object, counter, instance fields for counter .blg ingest

--- CollectSFDataDll/Kusto/KustoIngestionMappings.cs
+++ CollectSFDataDll/Kusto/KustoIngestionMappings.cs
@@ -36,7 +36,10 @@ namespace CollectSFData.Kusto
             {
                 new KustoCsvSchema() { Name = "Timestamp", DataType = "datetime", Ordinal = count++ },
                 new KustoCsvSchema() { Name = "CounterName", DataType = "string",  Ordinal = count++ },
-                new KustoCsvSchema() { Name = "CounterValue", DataType = "float",  Ordinal = count }
+                new KustoCsvSchema() { Name = "CounterValue", DataType = "float",  Ordinal = count++ },
+                new KustoCsvSchema() { Name = "Object", DataType = "string",  Ordinal = count++ },
+                new KustoCsvSchema() { Name = "Counter", DataType = "string",  Ordinal = count++ },
+                new KustoCsvSchema() { Name = "Instance", DataType = "string",  Ordinal = count }
             };
 
             return AddConstants(mapping);
