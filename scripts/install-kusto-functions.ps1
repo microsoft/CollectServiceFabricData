5d2d20e8d8fba4a7ea7a029697e5a7ab1ac70c1a
Jason Gilbertson <jagilber@microsoft.com>
2020-11-06 17:28:46 -0500

    add error checking

--- scripts/install-kusto-functions.ps1
+++ scripts/install-kusto-functions.ps1
@@ -2,31 +2,49 @@
     script to install kusto functions in .csl format using kusto-rest.ps1 
 #>
 param(
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$kustoCluster = '',
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$kustoDatabase = '',
+    [string]$location = '',
     [switch]$test,
     [switch]$force,
     [string]$kustoFunctionsDir = "$psscriptroot\..\KustoFunctions"
 )
 
-$erroractionpreference = 'continue'
+$ErrorActionPreference = 'continue'
 $error.clear()
-. .\kusto-rest.ps1 -cluster $kustoCluster -database $kustoDatabase
 
-$kustoScripts = [io.directory]::getFiles($kustoFunctionsDir, '*.csl',[io.searchoption]::AllDirectories)
+if (!$kusto) {
+    . .\kusto-rest.ps1 -cluster $kustoCluster -database $kustoDatabase
+}
 
-foreach($script in $kustoScripts) {
+$kustoScripts = [io.directory]::getFiles($kustoFunctionsDir, '*.csl', [io.searchoption]::AllDirectories)
+$scriptErrors = [collections.arraylist]::new()
+
+foreach ($script in $kustoScripts) {
     write-host "`$kusto.ExecScript(`"$script`")" -foregroundcolor cyan
 
-    if(!$test) {
-        $kusto.ExecScript("$script")
+    if (!$test) {
+        try {
+            $kusto.ExecScript("$script")
+        }
+        catch {
+            [void]$scriptErrors.Add($script)
+        }
     }
 
-    if($error -and !$force) {
+    if ($error -and !$force) {
         return
     }
+    else {
+        $error.Clear()
+    }
+}
+
+if ($scriptErrors) {
+    $scriptErrors | out-string
+    Write-Warning "the above scripts need to be executed manually:"
 }
 
 write-host 'finished'
