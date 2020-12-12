ef30fa070b29925d25c61d8cbf55f252dac11d0d
Jason Gilbertson <jagilber@microsoft.com>
2020-12-11 23:43:58 -0500

    add kustoqueuemessage

---
+++ src/CollectSFDataDll/Kusto/KustoSuccessMessage.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+using System;
+
+namespace CollectSFData.Kusto
+{    public class KustoSuccessMessage
+    {
+        public string Database;
+        public string FailureStatus;
+        public string IngestionSourceId;
+        public string IngestionSourcePath;
+        public string OperationId;
+        public string RootActivityId;
+        public DateTime SucceededOn;
+        public string Table;
+    }
+}
