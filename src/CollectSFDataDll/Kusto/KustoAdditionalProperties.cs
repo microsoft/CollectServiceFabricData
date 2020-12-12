03ce93f3647613f544051944017c100b44373a74
Jason Gilbertson <jagilber@microsoft.com>
2020-12-11 23:43:58 -0500

    add kustoqueuemessage

---
+++ src/CollectSFDataDll/Kusto/KustoAdditionalProperties.cs
@@ -0,0 +1,15 @@
+﻿// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
+namespace CollectSFData.Kusto
+{
+    public class KustoAdditionalProperties
+    {
+        public string authorizationContext;
+        public bool compressed;
+        public string csvMapping;
+        public string jsonMapping;
+    }
+}
