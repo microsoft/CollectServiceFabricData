4cf46a2043054d1b646360d5ff29705ce739097d
Jason Gilbertson <jagilber@microsoft.com>
2020-12-20 13:37:14 -0500

    testing log / customtaskmanager reset

--- src/CollectSFDataDll/Collector.cs
+++ src/CollectSFDataDll/Collector.cs
@@ -100,9 +100,9 @@ namespace CollectSFData
             }
             finally
             {
+                Log.Reset();
                 CustomTaskManager.Reset();
                 _noProgressTimer?.Dispose();
-                Log.Reset();
             }
         }
 
