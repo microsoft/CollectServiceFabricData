5b2e7a8cd8c747b32b4751ca36741b833209f77d
Jason Gilbertson <jagilber@microsoft.com>
2020-12-21 08:58:26 -0500

    fix for bad gathertype check from commandline

--- src/CollectSFDataDll/Common/ConfigurationOptions.cs
+++ src/CollectSFDataDll/Common/ConfigurationOptions.cs
@@ -91,10 +91,8 @@ namespace CollectSFData.Common
                 {
                     Log.Warning($"GatherType unknown: {value}");
                 }
-                else
-                {
-                    FileType = fileType;
-                }
+
+                FileType = fileType;
             }
         }
 
