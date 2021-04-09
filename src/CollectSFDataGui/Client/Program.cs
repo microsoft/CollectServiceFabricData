0b777fb235d548e7b34d3b82824979eb910e3538
Jason Gilbertson <jagilber@users.noreply.github.com>
2021-04-09 13:57:09 -0400

    client server working with ConfigurationProperties

--- src/CollectSFDataGui/Client/Program.cs
+++ src/CollectSFDataGui/Client/Program.cs
@@ -1,14 +1,14 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CollectSFDataGui
+namespace CollectSFDataGui.Client
 {
     public class Program
     {
