f883e5ad271d01106b6d9c6969c5328377343a7a
Jason Gilbertson <jagilber@users.noreply.github.com>
2021-04-10 09:25:04 -0400

    add EventConsole , wasm logging, upload button

--- src/CollectSFDataGui/Client/Program.cs
+++ src/CollectSFDataGui/Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace CollectSFDataGui.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Logging.SetMinimumLevel(LogLevel.Debug);
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
