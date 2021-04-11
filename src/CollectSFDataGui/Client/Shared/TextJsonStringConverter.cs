ee2214681efb40ffcc2cc7de93fa63c1b264ed32
Jason Gilbertson <jagilber@users.noreply.github.com>
2021-04-11 09:08:03 -0400

    add StringConverter override to bridge between json.net and system.text.json. may not be needed. helps with system.text.json strict varial types where int and bool cannot have quotes. add logging from csfd

---
+++ src/CollectSFDataGui/Client/Shared/TextJsonStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+
+namespace CollectSFDataGui.Shared
+{
+    public class StringConverter : System.Text.Json.Serialization.JsonConverter<string>
+    {
+        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                var stringValue = reader.GetInt32();
+                return stringValue.ToString();
+            }
+            else if (reader.TokenType == JsonTokenType.String)
+            {
+                return reader.GetString();
+            }
+
+            throw new System.Text.Json.JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value);
+        }
+    }
+}
