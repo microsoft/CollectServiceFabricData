53790bed7bb35ff11d9616267a1499c1f0fa1e17
Jason Gilbertson <jagilber@users.noreply.github.com>
2021-04-10 22:48:45 -0400

    testing upload. working on 2nd request

--- src/CollectSFDataGui/Server/Controllers/ConfigurationController.cs
+++ src/CollectSFDataGui/Server/Controllers/ConfigurationController.cs
@@ -2,25 +2,27 @@
 using CollectSFData.Common;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 
 namespace CollectSFDataGui.Server.Controllers
 {
+    // https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-5.0
     [ApiController]
     [Route("[controller]")]
+    [Produces("application/json")]
     public class ConfigurationController : ControllerBase
     {
         private static Collector _collector = new Collector(new string[0], false);
 
         private readonly ILogger<ConfigurationController> _logger;
 
-        private static ConfigurationOptions _config { get; set; }
+        private static ConfigurationOptions _config => _collector.Config;// { get; set; }
 
         static ConfigurationController()
         {
-            _config = _collector.Config;
         }
 
         public ConfigurationController(ILogger<ConfigurationController> logger)
@@ -32,40 +34,64 @@ namespace CollectSFDataGui.Server.Controllers
         [Route("/api")]
         public IEnumerable<JsonResult> Get()
         {
-            //_logger.LogWarning($"Get:enter:request:{resource}");
-            ConfigurationProperties var = _config.Clone();
-            string jsonString = JsonSerializer.Serialize(var);
-            _logger.LogWarning($"Get:enter:jsonString:{jsonString}");
+            ConfigurationOptions ConfigurationOptions = _config.Clone();
+            string jsonString = JsonSerializer.Serialize(ConfigurationOptions, GetJsonSerializerOptions());
 
-            //    return new List<JsonResult>() { new JsonResult(_config.Clone()) }.AsEnumerable();
-            return new List<JsonResult>() { new JsonResult(new ConfigurationProperties()) }.AsEnumerable();
+            _logger.LogInformation($"Get:enter:jsonString:{jsonString}");
+            return new List<JsonResult>() { new JsonResult(new ConfigurationOptions()) }.AsEnumerable();
         }
 
         [HttpGet]
         [Route("/api/configuration")]
         public IEnumerable<JsonResult> GetConfiguration()//(string resource = null)
         {
-            //_logger.LogWarning($"Get:enter:request:{resource}");
-            ConfigurationProperties var = _config.Clone();
-            string jsonString = JsonSerializer.Serialize(var);
-            _logger.LogWarning($"Get:enter:jsonString:{jsonString}");
+            ConfigurationOptions ConfigurationOptions = _config.Clone();
+            string jsonString = JsonSerializer.Serialize(ConfigurationOptions, GetJsonSerializerOptions());
+            _logger.LogInformation($"Get:enter:jsonString:{jsonString}");
 
-            //    return new List<JsonResult>() { new JsonResult(_config.Clone()) }.AsEnumerable();
-            return new List<JsonResult>() { new JsonResult(_config.Clone()) }.AsEnumerable();
+            JsonResult jsonResult = new JsonResult(ConfigurationOptions, GetJsonSerializerOptions());
+            jsonResult.ContentType = "application/json;charset=utf-8";
+
+            return new List<JsonResult>() { jsonResult }.AsEnumerable();
         }
 
         [HttpPost("/api/configuration/update")]
-        public IActionResult ImportConfiguration([FromBody] ConfigurationProperties properties)
+        public IActionResult ImportConfiguration([FromBody] string properties)
         {
-            // todo:implement
-            return Created($"update/{properties.GatherType}", properties);
+            try
+            {
+                ConfigurationOptions configurationProperties = JsonSerializer.Deserialize<ConfigurationOptions>(properties);
+                _config.MergeConfig(configurationProperties);
+
+                ConfigurationOptions newConfigurationOptions = _config.Clone();
+
+                JsonResult jsonResult = new JsonResult(newConfigurationOptions, GetJsonSerializerOptions());
+                jsonResult.ContentType = "application/json;charset=utf-8";
+                string jsonString = JsonSerializer.Serialize(newConfigurationOptions, GetJsonSerializerOptions());
+
+                return Created($"/api/configuration/update", jsonString);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"exception:{e}");
+                return BadRequest($"update/{properties} error:{e}");
+            }
         }
 
         [HttpGet]
         [Route("/api/index")]
-        public IEnumerable<ConfigurationProperties> Index()
+        public IEnumerable<ConfigurationOptions> Index()
         {
-            return new List<ConfigurationProperties>() { _config.Clone() }.AsEnumerable();
+            return new List<ConfigurationOptions>() { _config.Clone() }.AsEnumerable();
+        }
+
+        private JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions(JsonSerializerDefaults.General);
+            options.AllowTrailingCommas = true;
+            options.MaxDepth = 10;
+            options.PropertyNameCaseInsensitive = false;
+            return options;
         }
     }
 }
