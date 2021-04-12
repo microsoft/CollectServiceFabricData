d73e1cd70f8d7e74d60ce30bb3fd9d12157f3542
Jason Gilbertson <jagilber@microsoft.com>
2021-04-11 21:24:11 -0400

    ConfigurationProperties with Sas Info Deserialize working in client razor

--- src/CollectSFDataGui/Shared/TextJsonStringConverter.cs
+++ src/CollectSFDataGui/Shared/TextJsonStringConverter.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace CollectSFDataGui.Shared
 {
-    public class StringConverter : System.Text.Json.Serialization.JsonConverter<string>
-    {
-        public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            if (reader.TokenType == JsonTokenType.Number)
-            {
-                var stringValue = reader.GetInt32();
-                return stringValue.ToString();
-            }
-            else if (reader.TokenType == JsonTokenType.String)
-            {
-                return reader.GetString();
-            }
 
-            throw new System.Text.Json.JsonException();
+    public class JsonHelpers
+    {
+
+        public static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            options.Converters.Add(new JsonHelpers.StringConverter());
+            options.AllowTrailingCommas = true;
+            options.MaxDepth = 5;
+            //options.IncludeFields = true;
+            options.PropertyNameCaseInsensitive = true;
+            options.ReadCommentHandling = JsonCommentHandling.Skip;
+            //options.ReferenceHandler = ReferenceHandler.Preserve;
+            return options;
         }
 
-        public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+        public class StringConverter : System.Text.Json.Serialization.JsonConverter<string>
         {
-            writer.WriteStringValue(value);
+            public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType == JsonTokenType.Number)
+                {
+                    var stringValue = reader.GetInt32();
+                    return stringValue.ToString();
+                }
+                else if (reader.TokenType == JsonTokenType.String)
+                {
+                    return reader.GetString();
+                }
+
+                throw new System.Text.Json.JsonException();
+            }
+
+            public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options)
+            {
+                writer.WriteStringValue(value);
+            }
+
         }
     }
 }
