2108dce03eb1ebe124e013f7408a4918a9683603
Jason Gilbertson <jagilber@microsoft.com>
2021-04-23 21:59:01 -0400

    testing kv

--- src/CollectSFDataDll/Common/Constants.cs
+++ src/CollectSFDataDll/Common/Constants.cs
@@ -10,6 +10,7 @@ namespace CollectSFData.Common
     public class Constants
     {
         public const string ArmApiVersion = "api-version=2018-05-01";
+        public const string AzureKeyvaultSuffix = "vault.azure.net";
         public const string AzureStorageSuffix = "core.windows.net";
         public const string CodeLatestRelease = "https://api.github.com/repos/microsoft/CollectServiceFabricData/releases/latest";
         public const string CodeRepository = "https://github.com/microsoft/CollectServiceFabricData";
