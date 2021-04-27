f1b943a9437e9c973a866d063dfcbde8734097d7
Jason Gilbertson <jagilber@microsoft.com>
2021-04-27 07:19:26 -0400

    add CollectSFDataDllTest for dll testing

---
+++ src/CollectSFDataDllTests/Common/ConfigurationPropertiesTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CollectSFData.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectSFData.Common.Tests
+{
+    [TestClass()]
+    public class ConfigurationPropertiesTests
+    {
+        [TestMethod()]
+        public void ConfigurationPropertiesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
