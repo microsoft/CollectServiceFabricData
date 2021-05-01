75ef7534d4c2ed5cb37d78965b77849f3380d1c0
Jason Gilbertson <jagilber@microsoft.com>
2021-04-30 21:36:51 -0400

    remove os check

--- src/CollectSFData/Program.cs
+++ src/CollectSFData/Program.cs
@@ -13,11 +13,6 @@ namespace CollectSFData
     {
         private static int Main(string[] args)
         {
-            if (!Environment.Is64BitOperatingSystem | Environment.OSVersion.Platform != PlatformID.Win32NT)
-            {
-                Console.WriteLine("only supported on win32 x64");
-            }
-
             Collector collector = new Collector(true);
             ConfigurationOptions config = new ConfigurationOptions(args, true);
 
