226ccd1f901f7a7d18c9b4d5b00f9c2ff3d98266
Jason Gilbertson <jagilber@microsoft.com>
2021-05-11 08:24:53 -0400

    add tests for MapFileDataTypeExtension and refactor

---
+++ src/CollectSFDataDllTest/DataFile/FileTypesTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using CollectSFData.Common;
+using CollectSFData.DataFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectSFData.DataFile.Tests
+{
+    [TestFixture()]
+    public class FileTypesTests
+    {
+        [Test()]
+        public void MapFileDataTypeExtensionTest()
+        {
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("c:\\") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension(null) == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("noextension") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("./test.csv") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("./test.etl") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension("./test.json") == FileDataTypesEnum.unknown);
+
+            ExtensionTests(Constants.CsvExtension, FileDataTypesEnum.unknown);
+            ExtensionTests(Constants.DumpExtension, FileDataTypesEnum.fabriccrashdumps);
+            ExtensionTests(Constants.EtlExtension, FileDataTypesEnum.unknown);
+            ExtensionTests(Constants.JsonExtension, FileDataTypesEnum.unknown);
+            ExtensionTests(Constants.PerfCsvExtension, FileDataTypesEnum.counter);
+            ExtensionTests(Constants.PerfCtrExtension, FileDataTypesEnum.counter);
+            ExtensionTests(Constants.SetupExtension, FileDataTypesEnum.fabricsetup);
+            ExtensionTests(Constants.TableExtension, FileDataTypesEnum.table);
+            ExtensionTests(Constants.TraceFileExtension, FileDataTypesEnum.fabric);
+            ExtensionTests(Constants.TraceZipExtension, FileDataTypesEnum.fabric);
+            ExtensionTests(Constants.ZipExtension, FileDataTypesEnum.unknown);
+        }
+
+        [Test()]
+        public void MapFileDataTypeUriTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void MapFileTypeRelativeUriPrefixTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void MapFileTypeUriTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void MapFileUriTypeTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test()]
+        public void MapKnownFileExtensionTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ExtensionTests(string extension, FileDataTypesEnum dataType)
+        {
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"./test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"/test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"c:/test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"c:\\test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"c:\\temp/test{extension}") == dataType);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"./test/perf/csv/test{extension}") == dataType);
+
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"./test/perf/csv/test{extension}.test") == FileDataTypesEnum.unknown);
+            Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"{extension}.test") == FileDataTypesEnum.unknown);
+            //Assert.IsTrue(FileTypes.MapFileDataTypeExtension($"{extension}") == FileDataTypesEnum.unknown);
+        }
+    }
+}
