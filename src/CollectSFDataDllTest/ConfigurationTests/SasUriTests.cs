ace58250d0b065d487ba435b816aa6671f4a53fe
Jason Gilbertson <jagilber@microsoft.com>
2021-05-23 09:04:07 -0400

    add tests

--- src/CollectSFDataDllTest/ConfigurationTests/SasUriTests.cs
+++ src/CollectSFDataDllTest/ConfigurationTests/SasUriTests.cs
@@ -4,11 +4,12 @@
 // ------------------------------------------------------------
 
 using CollectSFData.Azure;
+using CollectSFDataDllTest.Utilities;
 using CollectSFDataTest.Utilities;
 using NUnit.Framework;
 using System;
 
-namespace CollectSFDataTest
+namespace CollectSFDataDll.ConfigurationTests
 {
     [TestFixture]
     public class SasUriTests : TestUtilities
@@ -33,13 +34,7 @@ namespace CollectSFDataTest
                 "https://sflogsxxxxxxxx.table.core.windows.net/fabriclogf33707ca75bd43c7aafbf589f66bed6cServices" +
                 "?sv=2015-04-05&ss=bt&srt=sco&sp=rl&se=2020-04-16T00:45:16.1725245Z&sig=5YDOE%2B1VSEGc5Te%2F%2B7w%2FK%2FXgZGmTL5%2FX9LNiCFKWWGU%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(true, endpoints.IsValid());
         }
 
@@ -49,13 +44,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/" +
                 "?sv=2019-10-10&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=oie5MRDFM9VLlCMye%2F%2FPFDkO4ZOdPsYT5auRlpve7sE%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(false, endpoints.IsValid());
         }
 
@@ -65,13 +54,8 @@ namespace CollectSFDataTest
             string sasUri = "sflogsxxxxxxxx.table.core.windows.net/" +
                 "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-04-21T02:40:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=0W03jW29bxVoYk0T480sbd0NsfEyOAw%2B%2F6mBHGl5fVY%3D";
 
-            StartConsoleRedirection();
             SasEndpoints endpoints = null;
-
-            WriteConsole($"checking uri for UriFormatException {sasUri}");
             Assert.Throws(typeof(UriFormatException), () => endpoints = new SasEndpoints(sasUri));
-
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
         }
 
         [Test(Description = "SAS no access sas fail test", TestOf = typeof(SasEndpoints))]
@@ -80,13 +64,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.file.core.windows.net/" +
                 "?sv=2019-10-10&ss=bfqt&srt=s&sp=acup&se=2025-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=AWXU0d737F%2BicH1Ht9vutSOdWmS3K6JEzCihLT0DjZc%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(false, endpoints.IsValid());
         }
 
@@ -96,13 +74,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.table.core.windows.net/" +
                 "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-04-21T10:28:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(false, endpoints.IsValid());
         }
 
@@ -115,13 +87,7 @@ namespace CollectSFDataTest
                 "TableEndpoint=https://sflogsxxxxxxxx.table.core.windows.net/;" +
                 "SharedAccessSignature=sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-04-21T10:28:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=quPGr6xzAvhli6KbLn%2BMm%2Fy2x0EsFw%2FipFvBiyiSLEU%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(true, endpoints.IsValid());
         }
 
@@ -131,13 +97,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/" +
                 "?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-04-21T10:28:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=quPGr6xzAvhli6KbLn%2BMm%2Fy2x0EsFw%2FipFvBiyiSLEU%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(true, endpoints.IsValid());
         }
 
@@ -148,13 +108,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/" +
                 "?sv=2015-02-21&st=2015-07-01T08%3a49%3a37.0000000Z&se=2025-07-02T08%3a49%3a37.0000000Z&sr=b&sp=rd&si=YWJjZGVmZw%3d%3d&sig=%2bSzBm0wi8xECuGkKw97wnkSZ%2f62sxU%2b6Hq6a7qojIVE%3d";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(true, endpoints.IsValid());
         }
 
@@ -162,16 +116,8 @@ namespace CollectSFDataTest
         public void SasStorageAccountOnlyFailTest()
         {
             string sasUri = "https://sflogsxxxxxxxx.table.core.windows.net/";
-            StartConsoleRedirection();
-            SasEndpoints endpoints = null;
-
-            WriteConsole($"checking uri {sasUri}", sasUri);
-            endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
+            SasEndpoints endpoints = new SasEndpoints(sasUri);
             Assert.AreEqual(false, endpoints.IsValid());
-
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
         }
 
         [Test(Description = "SAS write only sas fail test", TestOf = typeof(SasEndpoints))]
@@ -180,13 +126,7 @@ namespace CollectSFDataTest
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/" +
                 "?sv=2019-10-10&ss=bqt&srt=sco&sp=wacup&se=2025-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=HF7%2Fb5lV0M1pLCHExVFtQy5zdcMDvSh5WomJNIhzUOA%3D";
 
-            WriteConsole($"checking uri {sasUri}");
-            StartConsoleRedirection();
-
             SasEndpoints endpoints = new SasEndpoints(sasUri);
-            WriteConsole($"ProcessOutput", StopConsoleRedirection());
-            WriteConsole($"checking uri result {sasUri}", endpoints);
-
             Assert.AreEqual(false, endpoints.IsValid());
         }
     }
