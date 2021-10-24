0d897fb8937a041cb0eca7abe894226f4315ba75
Jason Gilbertson <jagilber@microsoft.com>
2021-10-24 18:49:59 -0400

    fix kusto scope from kusto.read to user_impersonation

--- scripts/kusto-rest.ps1
+++ scripts/kusto-rest.ps1
@@ -597,7 +597,8 @@ class KustoObj {
             write-verbose "token valid: $($this.authenticationResult.ExpiresOn). use -force to force logon"
             return $true
         }
-        return $this.LogonMsal($resourceUrl, @("$resourceUrl/kusto.read", "$resourceUrl/kusto.write"))
+        #return $this.LogonMsal($resourceUrl, @("$resourceUrl/kusto.read", "$resourceUrl/kusto.write"))
+        return $this.LogonMsal($resourceUrl, @("$resourceUrl/user_impersonation"))
     }
 
     hidden [bool] LogonMsal([string]$resourceUrl, [string[]]$scopes) {
