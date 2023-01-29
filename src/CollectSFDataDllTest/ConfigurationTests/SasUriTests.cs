07c05b6cb415a04c9acfc12a17cea6988e47f396
Jason Gilbertson <jagilber@users.noreply.github.com>
2023-01-29 14:31:01 -0500

    update test project modules and suppress messages. building with no errors.

--- src/CollectSFDataDllTest/ConfigurationTests/SasUriTests.cs
+++ src/CollectSFDataDllTest/ConfigurationTests/SasUriTests.cs
@@ -41,7 +41,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS expired sas fail test", TestOf = typeof(SasEndpoints))]
         public void SasExpiredFailTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/?sv=2019-10-10&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=oie5MRDFM9VLlCMye%2F%2FPFDkO4ZOdPsYT5auRlpve7sE%3D";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
@@ -51,7 +51,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS invalid uri fail test", TestOf = typeof(SasEndpoints))]
         public void SasInvalidUriFailTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "sflogsxxxxxxxx.table.core.windows.net/?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2020-04-21T02:40:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=0W03jW29bxVoYk0T480sbd0NsfEyOAw%2B%2F6mBHGl5fVY%3D";
 
             SasEndpoints endpoints = null;
@@ -61,7 +61,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS no access sas fail test", TestOf = typeof(SasEndpoints))]
         public void SasNoAccessFailTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.file.core.windows.net/?sv=2019-10-10&ss=bfqt&srt=s&sp=acup&se=2025-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=AWXU0d737F%2BicH1Ht9vutSOdWmS3K6JEzCihLT0DjZc%3D";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
@@ -71,7 +71,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS no signature sas fail test", TestOf = typeof(SasEndpoints))]
         public void SasNoSignatureFailTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.table.core.windows.net/?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-04-21T10:28:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
@@ -94,7 +94,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS portal full access test (default)", TestOf = typeof(SasEndpoints))]
         public void SasPortalFullAccessTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2025-04-21T10:28:09Z&st=2020-04-21T02:28:09Z&spr=https&sig=quPGr6xzAvhli6KbLn%2BMm%2Fy2x0EsFw%2FipFvBiyiSLEU%3D";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
@@ -105,7 +105,7 @@ namespace CollectSFData.ConfigurationTests
         public void SasServiceSasTest()
         {
             // service sas example has 'sr='
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/?sv=2015-02-21&st=2015-07-01T08%3a49%3a37.0000000Z&se=2025-07-02T08%3a49%3a37.0000000Z&sr=b&sp=rd&si=YWJjZGVmZw%3d%3d&sig=%2bSzBm0wi8xECuGkKw97wnkSZ%2f62sxU%2b6Hq6a7qojIVE%3d";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
@@ -123,7 +123,7 @@ namespace CollectSFData.ConfigurationTests
         [Test(Description = "SAS write only sas fail test", TestOf = typeof(SasEndpoints))]
         public void SasWriteOnlyFailTest()
         {
-            [SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="Test sas key")]
             string sasUri = "https://sflogsxxxxxxxx.blob.core.windows.net/?sv=2019-10-10&ss=bqt&srt=sco&sp=wacup&se=2025-04-22T12:10:19Z&st=2020-04-22T12:05:19Z&spr=https&sig=HF7%2Fb5lV0M1pLCHExVFtQy5zdcMDvSh5WomJNIhzUOA%3D";
 
             SasEndpoints endpoints = new SasEndpoints(sasUri);
