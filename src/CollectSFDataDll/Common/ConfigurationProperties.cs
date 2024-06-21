16a0b876e6d7a4a4a0987c584d278e04335b25a4
jagilber <jagilber@microsoft.com>
2024-06-21 07:37:40 -0400

    add 'common' as default tenant name. using tenant name in both ARM and non-ARM logons to kusto

--- src/CollectSFDataDll/Common/ConfigurationProperties.cs
+++ src/CollectSFDataDll/Common/ConfigurationProperties.cs
@@ -24,7 +24,7 @@ namespace CollectSFData.Common
 
         public string AzureSubscriptionId { get; set; }
 
-        public string AzureTenantId { get; set; }
+        public string AzureTenantId { get; set; } = "common";
 
         public string CacheLocation { get; set; }
 
