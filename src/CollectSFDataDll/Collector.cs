fe4a4d55cc90edabe9658b9f77636dabed2a4187
Victoria <t-vchin@microsoft.com>
2024-07-12 13:49:23 -0700

    edited if statement in FinalizeKusto to not be nested

--- src/CollectSFDataDll/Collector.cs
+++ src/CollectSFDataDll/Collector.cs
@@ -227,16 +227,13 @@ namespace CollectSFData
             {
                 Log.Warning($"there may have been errors during kusto import. {Config.CacheLocation} has *not* been deleted.");
             }
+            else if (Config.IsKustoConfigured() && Config.IsIngestionLocal)
+            {
+                Log.Last($"{Instance.Kusto.Endpoint.ClusterName}", ConsoleColor.Cyan);
+            }
             else if (Config.IsKustoConfigured())
             {
-                if (Config.IsIngestionLocal)
-                {
-                    Log.Last($"{Instance.Kusto.Endpoint.ClusterName}", ConsoleColor.Cyan);
-                }
-                else
-                {
-                    Log.Last($"{Constants.DataExplorer}/clusters/{Instance.Kusto.Endpoint.ClusterName}/databases/{Instance.Kusto.Endpoint.DatabaseName}", ConsoleColor.Cyan);
-                }
+                Log.Last($"{Constants.DataExplorer}/clusters/{Instance.Kusto.Endpoint.ClusterName}/databases/{Instance.Kusto.Endpoint.DatabaseName}", ConsoleColor.Cyan);
             }
 
             if (Instance.FileObjects.Any(FileStatus.failed | FileStatus.uploading))
